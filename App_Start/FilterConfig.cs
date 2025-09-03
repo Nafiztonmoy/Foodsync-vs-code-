ed9ebfd53d55b2f95a41c98293660e09f4c49308
Nafiztonmoy <129676456+Nafiztonmoy@users.noreply.github.com>
2025-09-03 19:49:06 +0600

    Initial commit: Foodsync project setup

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Foodsync_vs_code_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
